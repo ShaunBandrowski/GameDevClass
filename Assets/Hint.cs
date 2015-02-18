d82fcbebdbf6bd1bf82f44002a43666b6dd10941
Shaun Bandrowski <shaun.toro@gmail.com>
2015-02-17 20:32:59 -0500

    Completed Treasure Hunt game.

---
+++ Assets/Hint.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Hint : MonoBehaviour {
+
+
+	bool rescue = false;
+	bool hintOne = false;
+	bool hintTwo = false;
+
+	// Use this for initialization
+	void Start () {
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float dist = Vector3.Distance(GameObject.Find("Alien").transform.position,(GameObject.Find("UFO").transform.position));
+		float distTwo = Vector3.Distance(GameObject.Find("Drone").transform.position,(GameObject.Find("UFO").transform.position));
+		float distThree = Vector3.Distance(GameObject.Find("Crash").transform.position,(GameObject.Find("UFO").transform.position));
+		string textBuffer = "";
+
+		if (dist > 250){
+
+			if(hintOne == false && hintTwo == false&&rescue == false){
+
+			textBuffer += "Look for your fallen comrade and any clues that might help you find him!\n\nYou did hear a loud crash coming from the East...";
+
+			}else if(hintOne == true && hintTwo == false){
+
+				textBuffer += "Head North and look for more clues!";
+
+			}
+
+			if(hintTwo == true && dist <680 && rescue == false){
+
+				textBuffer += "Your comrade is somewhere nearby!";
+
+			}else if (hintTwo == true && dist > 680 && rescue == false){
+
+				textBuffer += "Head back near the crashed ship to look for your comrade!";
+
+			}
+
+			if ( distTwo < 300&& rescue == false){
+
+				textBuffer = "The fallen drone is emitting a signal that points North...";
+				hintOne = true;
+
+			}
+
+			if(distThree < 460 && rescue == false){
+
+				textBuffer = "This is your comrade's crashed ship!  He must be close...";
+				hintTwo = true;
+				hintOne = false;
+
+			}
+
+		}else if(dist<250&&rescue==false){
+
+			textBuffer += "You found him! Press [SPACE] to beam him up to your ship!";
+
+			if(Input.GetKeyDown(KeyCode.Space)){
+				hintOne = false;
+				hintTwo = false;
+				rescue = true;
+
+			}
+		
+		}
+
+		if (rescue == true){
+
+			textBuffer += "Congrats!  You rescued him!";
+
+		}
+		GetComponent<Text>().text = textBuffer;  
+
+	}
+}
---
+++ Assets/UFOMove.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class UFOMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//move forward if I press [W]
+		//GetKeyDown = having to press the button over and over 
+		//GetKey = will move as long as that button is held down
+		if (Input.GetKey ( KeyCode.W ) ) {
+			
+			//Time.deltaTime is the fraction of a second in between a frame
+			//A value that gets bigger with low FPS, but smaller with high FPS
+			transform.position += new Vector3 (0f, 0f, 100f) * Time.deltaTime;
+		}
+		
+		//FRAMERATE - DEPENDENT
+		//IF I HAVE MORE FPS, I GO FASTER
+		
+		//FRAMERATE - INDEPENDENT
+		//NO MATTER WHAT YOUR FRAMERATE IS, YOU GET THE SAME BEHAVIOR
+		//Movement should be framerate independent
+		
+		//move backward if I press [S]
+		if (Input.GetKey ( KeyCode.S ) ) {
+			
+			//Time.deltaTime is the fraction of a second in between a frame
+			//A value that gets bigger with low FPS, but smaller with high FPS
+			transform.position += new Vector3 (0f, 0f, -100f) * Time.deltaTime;
+		}
+		
+		//move left if I press [A]
+		if (Input.GetKey ( KeyCode.A ) ) {
+			
+			//Time.deltaTime is the fraction of a second in between a frame
+			//A value that gets bigger with low FPS, but smaller with high FPS
+			transform.position += new Vector3 (-100f, 0f, 0f) * Time.deltaTime;
+		}
+		
+		//move right if I press [D]
+		if (Input.GetKey ( KeyCode.D ) ) {
+			
+			//Time.deltaTime is the fraction of a second in between a frame
+			//A value that gets bigger with low FPS, but smaller with high FPS
+			transform.position += new Vector3 (100f, 0f, 0f) * Time.deltaTime;
+		}
+
+
+	
+	}
+}
